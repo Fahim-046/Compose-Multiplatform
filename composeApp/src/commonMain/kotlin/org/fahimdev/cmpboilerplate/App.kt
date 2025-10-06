70825a9987091aaab5d15205bbe7f74e3e5eb0a9
fahim-fardous <167993840+fahim-fardous@users.noreply.github.com>
2025-10-06 16:48:09 +0600

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/org/fahimdev/cmpboilerplate/App.kt
@@ -0,0 +1,34 @@
+package org.fahimdev.cmpboilerplate
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import cmpboilerplate.composeapp.generated.resources.Res
+import cmpboilerplate.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text("Hello World!")
+        }
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/fahimdev/cmpboilerplate/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.fahimdev.cmpboilerplate
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
